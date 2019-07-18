7a6386fda2464feba62b8906ceca9a1b79053c19
saiprasannasastry <ssastry@infoblox.com>
2019-07-18 22:37:44 +0530

    Adding same naming pattern for HA routers as normal routers
    Change-Id: Icebb8287d6c72ab18d83bf49a13260105e842843

--- networking_infoblox/neutron/common/constants.py
+++ networking_infoblox/neutron/common/constants.py
@@ -160,8 +160,6 @@ IS_DEFAULT = 'is_default'
 NEUTRON_DEVICE_OWNER_TO_PATTERN_MAP = {
     n_const.DEVICE_OWNER_DHCP: 'dhcp-port-{ip_address}',
     n_const.DEVICE_OWNER_ROUTER_INTF: 'router-iface-{ip_address}',
-    n_const.DEVICE_OWNER_ROUTER_HA_INTF: 'router-ha-iface-{ip_address}',
-    n_const.DEVICE_OWNER_HA_REPLICATED_INT: 'router-ha-iface-{ip_address}',
     n_const.DEVICE_OWNER_ROUTER_GW: 'router-gw-{ip_address}',
     n_const.DEVICE_OWNER_FLOATINGIP: 'floating-ip-{ip_address}',
     'neutron:' + p_const.LOADBALANCER: 'lb-vip-{ip_address}'
@@ -170,8 +168,6 @@ NEUTRON_DEVICE_OWNER_TO_PATTERN_MAP = {
 NEUTRON_INTERNAL_SERVICE_DEVICE_OWNERS = [
     n_const.DEVICE_OWNER_DHCP,
     n_const.DEVICE_OWNER_ROUTER_INTF,
-    n_const.DEVICE_OWNER_ROUTER_HA_INTF,
-    n_const.DEVICE_OWNER_HA_REPLICATED_INT,
     n_const.DEVICE_OWNER_ROUTER_GW,
     'neutron:' + p_const.LOADBALANCER
 ]
